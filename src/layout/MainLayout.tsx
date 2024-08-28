c4621f5599272e8b69094deec591f47253d32eb2
LENZOZ <lorenzo.baeza.n@gmail.com>
2024-08-28 17:15:34 -0400

    css provisional

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
-import Homepage from '../pages/Homepage'
+import Homepage from '../pages/HomePage'
 import FormularioRegistroUsuario from '../components/FormularioRegistroUsuario'
 import FormularioRegistroMascota from '../components/FormularioRegistroMascota'
 import FormularioRegistroProducto from '../components/FormularioRegistroProducto';
-
+import  '../index.css';
 export default function MainLayout() {
   return (
     <>
