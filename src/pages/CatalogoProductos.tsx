1eb3896a7b41592027e0ff7e7a0de3b93d8d55b5
Claudia <cmoragrega@jri.cl>
2024-09-12 12:22:52 -0300

    actualización de css para dar estilo a las vistas

--- src/pages/CatalogoProductos.tsx
+++ src/pages/CatalogoProductos.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { CreateProductoDto } from "../interface/CreateProductoDTO";
+import "../index.css";
 
 const CatalogoProductos: React.FC = () => {
   const [productos, setProductos] = useState<CreateProductoDto[]>([]);
@@ -26,17 +27,30 @@ const CatalogoProductos: React.FC = () => {
 
   return (
     <>
-      <h1>Catalogo de Productos</h1>
-      <br />
-      <div className="producto-grid">
-        {productos.map((producto) => (
-          <div key={producto.id} className="producto-card">
-            <img src={producto.imagenes[0]} alt={producto.nombre} width="100" />
-            <h3>{producto.nombre}</h3>
-            <p>Precio: ${producto.precio}</p>
-            <Link to={`/detalle-productos/${producto.id}`}>Ver Detalle</Link>
-          </div>
-        ))}
+      <div className="catalogo-container">
+        <h1>Catalogo de Productos</h1>
+        <br />
+        <div className="producto-container">
+          {productos.map((producto) => (
+            <div key={producto.id} className="card-producto">
+              <img
+                src={producto.imagenes[0]}
+                alt={producto.nombre}
+                width="200"
+              />
+              <h3>{producto.nombre}</h3>
+              <p>Precio: ${producto.precio}</p>
+              <button className="btn-detalle">
+                <Link
+                  to={`/detalle-productos/${producto.id}`}
+                  className="link-detalle"
+                >
+                  Ver Detalle
+                </Link>
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
